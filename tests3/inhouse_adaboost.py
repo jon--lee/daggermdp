641ee7f1b87b6774c7d1e919e131b4b2ec56c824
Jonathan Lee <123abcjonathanlee@gmail.com>
2016-06-28 14:58:44 -0700

    update adaboost tests

--- tests3/inhouse_adaboost.py
+++ tests3/inhouse_adaboost.py
@@ -1,4 +1,4 @@
-#from adaboost import Adaboost
+from adaboost import Adaboost
 from policy import Action, DumbPolicy, ClassicPolicy
 from state import State
 from mdp import ClassicMDP
@@ -41,9 +41,9 @@ if not os.path.exists(data_directory):
 
 
 
-ITER = 20
-TRIALS = 3
-SAMP = 10
+ITER = 25
+TRIALS = 15
+SAMP = 20
 LIMIT_DATA = 1
 DEPTH = 6
 
@@ -80,49 +80,50 @@ class SVC_boosted():
         self.delta_stop = 1e-5
         self.phi = .5
 
-    def fit(self, features, labels):
-		'''
-    performs the adaboost code using classification
-    '''
-    # features = np.array([point[0] for point in data])
-    # labels = np.array([labels[1] for point in l])
-    weak_learners = []
-    learner_weights = []
-    data_weights = np.array([1.0/len(labels) for _ in range(len(labels))])
-    i = 0
-    while i < 100:
-        learner = SVC(kernel='linear') # instantiate a new weak learner
-        learner.optimize(X, labels, data_weights)
-        err = learner.predict(X, labels, data_weights) 
-        # results = learner.accuracy(data_embedding) # results is expected to an array of classes
-        raw_errors = np.equal(results, labels)
-        err = np.sum((1-raw_errors)  * data_weights)
+    def fit(self, X, labels):
+        '''
+        performs the adaboost code using classification
+        '''
+        # features = np.array([point[0] for point in data])
+        # labels = np.array([labels[1] for point in l])
+        weak_learners = []
+        learner_weights = []
+        data_weights = np.array([1.0/len(labels) for _ in range(len(labels))])
+        i = 0
+        while i < 10:
+            learner = SVC(kernel='linear') # instantiate a new weak learner
+            #learner = DecisionTreeClassifier(max_depth=3)
+            learner.fit(X, labels, sample_weight=data_weights)
+            err = learner.predict(X) 
+            # results = learner.accuracy(data_embedding) # results is expected to an array of classes
+            raw_errors = np.equal(err, labels)
+            err = np.sum((1-raw_errors)  * data_weights)
 
-        # get the weight of the learner
-        learner_weight = np.log2(self.phi*(1-err)/(err*(1-self.phi)))
-        learner_weights.append(learner_weight)
+            # get the weight of the learner
+            learner_weight = np.log2(self.phi*(1-err)/(err*(1-self.phi)))
+            learner_weights.append(learner_weight)
 
-        data_weights = data_weights*np.exp(-learner_weight*raw_errors)
-        normalization = np.sum(data_weights)
-        data_weights = data_weights/normalization
+            data_weights = data_weights*np.exp(-learner_weight*raw_errors)
+            normalization = np.sum(data_weights)
+            data_weights = data_weights/normalization
 
-        if err >= self.phi - self.delta_stop or err == 0:
-            break
-        i += 1
-        weak_learners.append(learner)
+            if err >= self.phi - self.delta_stop or err == 0:
+                break
+            i += 1
+            weak_learners.append(learner)
 
-    self.learners = weak_learners
-    self.learner_weights = learner_weights/np.sum(np.array(learner_weights))
+        self.learners = weak_learners
+        self.learner_weights = learner_weights/np.sum(np.array(learner_weights))
 
-	def predict(self, X):
-		predictions = np.zeros(X.shape[0])
-		for learner, weight in zip(self.learners, self.learner_weights):
-			predictions += learner.predict(X) * self.learner_weights
-		prediction_values = np.around(predictions)
-		prediction_values[prediction_values > 3] = 3
-		prediction_values[prediction_values < -1] = -1
-		return prediction_values
-	
+    def predict(self, X):
+        predictions = np.zeros(len(X))
+        for learner, weight in zip(self.learners, self.learner_weights):
+            predictions += learner.predict(X) * weight
+        prediction_values = np.around(predictions)
+        prediction_values[prediction_values > 3] = 3
+        prediction_values[prediction_values < -1] = -1
+        return prediction_values
+
 
 
 
@@ -132,6 +133,7 @@ for t in range(TRIALS):
     mdp.load_policy('scen4.p')
 
     svm = SVC(kernel='linear')
+    #dt = DecisionTreeClassifier(max_depth=3)
     sup = ScikitSupervise(grid, mdp, Classifier=svm)
     sup.sample_policy()
 
@@ -189,8 +191,9 @@ for t in range(TRIALS):
     print "\nAdaboost IL Trial: " + str(t)
     mdp.load_policy('scen4.p')
 
-    svm = SVC(kernel='linear')
-    ada = AdaBoostClassifier(svm, n_estimators=5, algorithm='SAMME')
+    #svm = SVC(kernel='linear')
+    #ada = AdaBoostClassifier(svm, n_estimators=5, algorithm='SAMME')
+    ada = SVC_boosted()
     sup = ScikitSupervise(grid, mdp, Classifier=ada)
     sup.sample_policy()
 
@@ -232,6 +235,62 @@ for t in range(TRIALS):
     ada_il_loss[t,:] = loss
 
 
+# SKLEARN adaboost
+
+sk_il_data = np.zeros([TRIALS, ITER])
+sk_il_acc = np.zeros([TRIALS, ITER])
+sk_il_loss = np.zeros([TRIALS, ITER])
+
+
+for t in range(TRIALS):
+    print "\nSKLearn adaboost IL Trial: " + str(t)
+    mdp.load_policy('scen4.p')
+
+    svm = SVC(kernel='linear')#DecisionTreeClassifier(max_depth=3)#SVC(kernel='linear')
+    ada = AdaBoostClassifier(svm, n_estimators=10, algorithm='SAMME')
+    #ada = SVC_boosted()
+    sup = ScikitSupervise(grid, mdp, Classifier=ada)
+    sup.sample_policy()
+
+    value_iter_analysis = Analysis(W, H, ITER, rewards=rewards, sinks=sinks,
+            desc='Value iter policy')
+    sk_il_r = np.zeros(ITER)
+    acc = np.zeros(ITER)
+    loss = np.zeros(ITER)
+
+    sup.record = True
+    #for _ in range(4):
+    #    sup.rollout()
+
+    for i in range(ITER):
+        print "     Iteration: " + str(i)
+        mdp.pi = value_iter_pi 
+        sup.record = True
+        for _ in range(SAMP):
+            if _  >= LIMIT_DATA:
+                sup.record = False
+            sup.rollout()
+
+        sup.record = False
+        print "     Training on " + str(len(sup.net.data)) + " examples"
+        sup.train()
+
+        acc[i] = sup.svm.acc()
+        for _ in range(SAMP):
+            sup.record=False
+            sup.rollout()
+            loss[i] += sup.get_loss() / float(SAMP)
+            sk_il_r[i] += sup.get_reward() / SAMP
+        #print acc        
+    if t == 0:
+        plotter.plot_state_actions(mdp.pi, rewards=rewards, sinks=sinks,
+                filename=comparisons_directory + 'sk_il_state_action.png')        
+    sk_il_data[t,:] = sk_il_r
+    sk_il_acc[t,:] = acc
+    sk_il_loss[t,:] = loss
+
+
+
 
 
 
@@ -243,30 +302,36 @@ for t in range(TRIALS):
 np.save(data_directory + 'sup_data.npy', value_iter_data)
 np.save(data_directory + 'svm_classic_il_data.npy', svm_il_data)
 np.save(data_directory + 'ada_classic_il_data.npy', ada_il_data)
+np.save(data_directory + 'sk_classic_il_data.npy', ada_il_data)
+
 
 np.save(data_directory + 'ada_il_acc.npy', ada_il_acc)
 np.save(data_directory + 'svm_il_acc.npy', svm_il_acc)
+np.save(data_directory + 'sk_il_acc.npy', svm_il_acc)
 
 analysis = Analysis(H, W, ITER, rewards=rewards, sinks=sinks, desc="General comparison")
 analysis.get_perf(value_iter_data)
 analysis.get_perf(svm_il_data)
 analysis.get_perf(ada_il_data)
+analysis.get_perf(sk_il_data)
 
 #analysis.plot(names = ['Value iteration', 'Adaboost IL'], filename=comparisons_directory + 'svm_reward_comparison.png', ylims=[-60, 100])
-analysis.plot(names = ['Value iteration', 'LSVM IL', 'LSVM Boosted IL'], filename=comparisons_directory + 'svm_reward_comparison.png', ylims=[-60, 100])
+analysis.plot(names = ['Value iteration', 'LSVM IL', 'LSVM Boosted IL', 'LSVM SK Boosted'], filename=comparisons_directory + 'svm_reward_comparison.png', ylims=[-60, 100])
 print "Saving analysis to: " + comparisons_directory + 'svm_reward_comparison.png'
 
 acc_analysis = Analysis(H, W, ITER, rewards = grid.reward_states, sinks=grid.sink_states, desc="Accuracy comparison")
 acc_analysis.get_perf(svm_il_acc)
 acc_analysis.get_perf(ada_il_acc)
+acc_analysis.get_perf(sk_il_acc)
 
-acc_analysis.plot(names = ['LSVM Acc.', 'LSVM Boosted Acc.'], label='Accuracy', filename=comparisons_directory + 'svm_acc_comparison.png', ylims=[0,1])
+acc_analysis.plot(names = ['LSVM Acc.', 'LSVM Boosted Acc.', 'LSVM SK Boosted'], label='Accuracy', filename=comparisons_directory + 'svm_acc_comparison.png', ylims=[0,1])
 #acc_analysis.plot(names = ['Adaboost IL Acc.'], label='Accuracy', filename=comparisons_directory + 'svm_acc_comparison.png', ylims=[0,1])
 
 loss_analysis = Analysis(H, W, ITER, rewards=rewards, sinks=sinks, desc="Loss plot")
 loss_analysis.get_perf(svm_il_loss)
 loss_analysis.get_perf(ada_il_loss)
-loss_analysis.plot(names = ['LSVM loss', 'LSVM Boosted loss'], filename=comparisons_directory + 'loss_plot.png', ylims=[0, 1])
+loss_analysis.get_perf(sk_il_loss)
+loss_analysis.plot(names = ['LSVM loss', 'LSVM Boosted loss', 'LSVM SK Boosted loss'], label='Loss', filename=comparisons_directory + 'loss_plot.png', ylims=[0, 1])
 
 
 
