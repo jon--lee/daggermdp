f1fb58218192be285804331a879888ca0497eeb7
mdlaskey <mdlaskey@umich.edu>
2016-06-28 11:21:44 -0700

    inhouse adaboost first impl

--- tests3/inhouse_adaboost.py
+++ tests3/inhouse_adaboost.py
@@ -37,3 +37,36 @@ if not os.path.exists(comparisons_directory):
 if not os.path.exists(data_directory):
     os.makedirs(data_directory)
 
+def fit_class_SVC(data, delta_stop):
+	'''
+    performs the adaboost code using classification
+    '''
+    features = np.array([point[0] for point in data])
+    labels = np.array([labels[1] for point in data])
+    weak_learners = []
+    learner_weights = []
+    data_weights = np.array([1.0/len(labels) for _ in range(len(labels))])
+    i = 0
+    while i < 100:
+        learner = SVC() # instantiate a new weak learner
+        learner.optimize(X, labels, data_weights)
+        err = learner.predict(X, labels, data_weights) 
+        # results = learner.accuracy(data_embedding) # results is expected to an array of classes
+        raw_errors = np.equal(results, labels)
+        err = np.sum((1-raw_errors)  * data_weights)
+
+        # get the weight of the learner
+        learner_weight = np.log2(phi*(1-err)/(err*(1-phi)))
+        learner_weights.append(learner_weight)
+
+        data_weights = data_weights*np.exp(-learner_weight*raw_errors)
+        normalization = np.sum(data_weights)
+        data_weights = data_weights/normalization
+
+        if err >= phi - delta_stop or err == 0:
+            break
+        i += 1
+        weak_learners.append(learner)
+
+    ensemble = weak_learners
+    weights = learner_weights/np.sum(np.array(learner_weights))
